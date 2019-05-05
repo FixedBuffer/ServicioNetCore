ccd7c146eb41a6f9c396e37d8711bd60b465839e
JorTurFer <jorge_turrado@hotmail.es>
2019-05-06 00:01:15 +0200

    Agregar archivos de proyecto.

---
+++ PostServicioNetCore/LifetimeHostedService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PostServicioNetCore
+{
+    public class LifetimeHostedService : IHostedService, IDisposable
+    {
+        ILogger<LifetimeHostedService> logger;
+        private Timer _timer;
+        private string _path;
+        public LifetimeHostedService(ILogger<LifetimeHostedService> logger, IHostingEnvironment hostingEnvironment)
+        {
+            this.logger = logger;
+            _path = $"{hostingEnvironment.ContentRootPath}PostServicioCore.txt"; 
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _timer = new Timer((e) => WriteTimeToFile(), null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"{ex.Message},{ex.StackTrace}");
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private void WriteTimeToFile()
+        {
+            WriteToFile(DateTime.Now.ToString());
+        }
+
+        private void WriteToFile(string message)
+        {
+            if (!File.Exists(_path))
+            {
+                using (var sw = File.CreateText(_path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (var sw = File.AppendText(_path))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ PostServicioNetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PostServicioNetCore
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var IsWindowsService = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !Debugger.IsAttached;
+
+            var host = new HostBuilder()
+                 .ConfigureHostConfiguration(configHost =>
+                 {
+                     //Configuración del host
+                 })
+                 .ConfigureAppConfiguration((hostContext, configApp) =>
+                 {
+                     //Configuración de la aplicacion
+
+                 })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    //Configuración de los servicios
+                    services.AddHostedService<LifetimeHostedService>();
+                });
+            if (IsWindowsService) //En caso de estar en windows y sin debuger, añadimos al a injeccion de dependencias el servicio
+            {
+                await host.RunServiceAsync();
+            }
+            else
+            {
+                await host.RunConsoleAsync();
+            }          
+        }
+    }
+}
